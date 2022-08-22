15b209f1f47873f6a957fe50667446f2331da2f1
rschlemer <rschlem1@gmail.com>
2022-08-21 20:28:25 -0500

    area fill

--- src/charts/AreaChart.svelte
+++ src/charts/AreaChart.svelte
@@ -1,10 +1,22 @@
 <script>
     import { scaleLinear } from "d3-scale";
+    import { area } from "d3-shape";
     import Axis from "./Axis.svelte";
     import ScatterPlot from "./ScatterPlot.svelte";
     import temp_data from "../assets/data_test.json";
 
     let data = temp_data.temp_data;
+
+    // line function
+    function f(x) {
+        return (225 - x * x) ** (1 / 2);
+        // return (-2 * x) / 3 + 10;
+    }
+
+    let lineData = [];
+    for (let i = 0; i < 31; i++) {
+        lineData.push({ x: i * 0.5, y: f(i * 0.5) });
+    }
 
     const margin = {
         top: 50,
@@ -26,11 +38,21 @@
     $: yScale = scaleLinear()
         .domain([0, 25])
         .range([innerHeight + margin.top, margin.top]);
+
+    $: path = area()
+        .x(function (d) {
+            return xScale(d.x);
+        })
+        .y0(height - margin.top)
+        .y1(function (d) {
+            return yScale(d.y);
+        });
 </script>
 
 <div class="areaChart" bind:clientWidth={width} bind:clientHeight={height}>
     <svg {width} {height}>
         <ScatterPlot {data} {xScale} {yScale} />
+
         <!-- Bottom Axis -->
         <Axis {innerHeight} {margin} scale={xScale} position="bottom" />
         <text
@@ -38,6 +60,7 @@
             x={margin.left + 25}
             y={innerHeight + margin.top + 35}>Spot Size</text
         >
+
         <!-- Left Axis -->
         <Axis {innerHeight} {margin} scale={yScale} position="left" />
         <text
@@ -46,5 +69,13 @@
                 innerHeight + margin.top - 25
             }) rotate(-90)`}>Spike Length</text
         >
+
+        <path class="pathArea" d={path(lineData)} />
     </svg>
 </div>
+
+<style>
+    .pathArea {
+        fill: #0000ff99;
+    }
+</style>
